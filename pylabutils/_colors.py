ea595673c67b5adad9956a5784e2d7500fcbaaf6
Mario García <mariogarcc@gmail.com>
2018-12-16 00:05:28 +0100

    Adding first version of _colors.py

---
+++ pylabutils/_colors.py
@@ -0,0 +1,24 @@
+"""
+Auxilliary list of HEX colors for graphic purposes.
+
+Recommended usage, if wanted:
+
+`from pylabutils._colors import color_dict [as <name>]`
+"""
+
+__all__ = ['color_dict']
+
+color_dict = {
+    'blue' : '#3385FF',
+    'red' : '#FB4A24',
+    'yellow' : '#EDF92B',
+    'green' : '#0DC40F',
+    'orange' : '#FF884D',
+    'purple' : '#CC66FF',
+    'brown' : '#B37700',
+    'black' : '#000000',
+}
+
+# To do:
+# add more colors, as well as light and dark
+# versions for existing ones might be added
