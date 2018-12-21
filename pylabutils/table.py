82b0222878abd7ba3f554b833df5f97b2b0bfe88
Mario García <mariogarcc@gmail.com>
2018-12-21 20:37:21 +0100

    Updating table.py to first beta release.
    Test for bugs and decide whether to implement
    the features proposed at the end of the code.

--- pylabutils/table.py
+++ pylabutils/table.py
@@ -1,96 +1,288 @@
-from IPython.display import display, Latex
+__all__ = ['table']
 
-def table(data, data_titles, precision, shape = 'ver', box = 'ver'):
-    '''Generates a simple Latex table.
+_values = dict(
+    shape = ('v', 'h', 'ver', 'hor', 'vertical', 'horizontal'),
+    sep = ('v', 'h', 'f', 'no', 'ver', 'hor', # 'n' is also in 'horizontal'
+        'vertical', 'horizontal', 'full', 'none'
+        ),
+    fit = ('h', 'h!', 'b', 't', 'H'),
+    loc = ('c', 'l', 'r', 'center', 'left', 'right',
+        'centering', 'raggedleft', 'raggedright'
+        ),
+    font_size_type = (
+        'Huge', 'huge', 'LARGE', 'Large',
+        'normalsize', 'small', 'footnotesize',
+        'scriptsize', 'tiny', None,
+        ),
+    )
+
+
+def table(data, data_titles, **options):
+    """
+    Generates a simple Latex table containing numeric data.
+
+    Parameters:
+
+    data : *N x array-like*
+    Data that will be presented in the table.
+
+    data_titles : *N x str, array-like*
+    Titles for the given data.
+
+    prec : *int or N x int, array-like; optional*
+    Specifies the amount of decimal digits to show on a decimal representation
+    of a number. Can be set to a list of values corresponding to one value for
+    each data list. If set to a single value, it will apply that value to
+    all lists of data.
+    Negative values can be used to round integers.
+    default : 2 for every list that has a decimal point, 0 otherwise
+
+    shape : *str; optional*
+    Chooses whether the table should be of vertical or horizontal layout.
+    default: 'vertical'
+
+    sep : *str; optional*
+    Specifies which kind of separation lines between elements are to be had
+    inside the table.
+    default : 'horizontal'
+
+    fit : *str; optional*
+    Specifies which style of text fit should be applied to the table.
+    default : 'H'
+
+    caption : *bool, None or str; optional*
+    Chooses whether to print the "\caption" command inside the table. Can be
+    provided with a string argument to put inside the command.
+    default : None
+
+    label : *bool, None or str; optional*
+    Chooses whether to print the "\label" command inside the table. Can be
+    provided with a string argument to put inside the command.
+    default : None
+
+    exp : *bool; optional*
+
+    exp_prec : *int or N x int, array-like; optional*
+    Specifies the amount of decimal digits to show on a power-of-ten
+    (scientific) representation of a number. Can be set to a list of values
+    corresponding to one value for each data list. If set to a single value,
+    it will apply that value to all lists of data.
+    Can only take values greater than or equal to 0.
+    default : 2
+
+    font_size_type : *str or None; optional*
+    Changes font size inside table.
+    default : None
 
     'data', 'data_titles' and 'precision' should all be tuples and have the same item order.
-    'shape' values: 'hor' or 'ver'; 'box' values: 'hor', 'ver', 'full' or 'none'.
-    'precision' values must be strings: '3.5f'; '.3i'; '.2f'; '.f' '''
+    'shape' values: 'hor' or 'ver'; 'sep' values: 'hor', 'ver', 'full' or 'none'.
+    'precision' values must be strings: '3.5f'; '.3i'; '.2f'; '.f'
+    """
 
-    shape_values = ('ver', 'hor')
-    box_values = ('full', 'none', 'ver', 'hor')
-    if shape not in shape_values:
-        raise(ValueError('shape value is incorrect'))
-        return
-    if box not in box_values:
-        raise(ValueError('box value is incorrect'))
-        return
+
+    kwargs = dict(
+        prec = [2 if any(['.' in str(num) for num in data_list]) \
+            else 0 for data_list in data],
+        shape = 'vertical',
+        sep = 'horizontal',
+        loc = 'centering',
+        fit = 'H',
+        caption = None,
+        label = None,
+        exp = False,
+        exp_prec = 2,
+        font_size_type = None,
+    )
+
+    options.update({k: v.lower() for k, v in
+        [(k, v) for k, v in options.items()][:4] if type(v) == str})
+    # maybe beautifyable, but gets job done. gives freedom at naming kwargs
+
+    kwargs.update(options)
+
+    if any([kwargs[option] not in _values[option] \
+            for option in _values.keys()]): # fast check
+        for option in _values.keys():
+            if kwargs[option] not in _values[option]:
+                raise ValueError("'{}' is not a valid value for '{}'" \
+                    .format(kwargs[option], option))
+                return
+
     else:
-        if len(data) != len(precision) or len(data) != len(data_titles):
-            raise(ValueError('data\'s, data titles\' and precision\'s sizes don\'t match'))
+        if len(data) == 1:
+            data = data[0]
+
+        if len(data) != len(kwargs['prec']) or len(data) != len(data_titles):
+            raise ValueError("data's, data titles' and" \
+                "precisions' sizes don't match")
             return
         else:
             for i in range(len(data)):
                 if len(data[i]) != len(data[i-1]):
-                    raise(ValueError('data\'s sizes don\'t match'))
+                    raise ValueError("data items' sizes don't match")
                     return
-                else:
-                    pass
 
-    cadena  = '\\begin{table}[H]\n\\begin{tabular}'
+    # argument validation
 
-    disp = ''
-    tt = ''
+    head  = '' \
+        + '\\begin{{table}}[{}]\n'.format(kwargs['fit']) \
+        + '\\{}\n'.format(kwargs['font_size_type']) \
+            * (kwargs['font_size_type'] is not None) \
+        + '\\{}\n'.format(kwargs['loc']) \
+        + '\\begin{tabular}'
+        # obligatory
 
-    if shape == 'ver':
-        if box == 'none' or box == 'hor':
-            disp += '{' + 'c' * len(data) + '}'
-        else:
-            disp += '{' + '|c' * len(data) + '|}'
-        cadena += disp
-        tt += '\n\\hline\n'
+    prec = kwargs['prec']
+    exp_prec = kwargs['exp_prec']
+
+
+    if kwargs['shape'] in 'vertical':
+
+        disp = '' \
+            + '{' \
+            + ('|' * (kwargs['sep'] in ('vertical''||''full')) + 'c') \
+            * len(data) \
+            + '|' * (kwargs['sep'] in ('vertical''||''full')) \
+            + '}'
+
+
+        titles = '' \
+            + '\n' \
+            + '\\hline\n' * (kwargs['sep'] in ('horizontal''||''full'))\
+
         for i in range(len(data_titles)):
-            tt += data_titles[i]
+            titles += data_titles[i]
             if i != len(data_titles) - 1:
-                tt += ' & '
+                titles += ' & '
             else:
-                tt +=  '\\\\\n\\hline\n'
-        cadena += tt
+                titles += '' \
+                    + '\\''\\''\n' \
+                    + '\\hline\n'
+
+        rows = ''
         for i in range(len(data[0])):
-            dt = '('
-            pt = ''
+            row = ''
             for j in range(len(data)):
-                dt += 'data[' + str(j) + '][' + str(i) + ']'
-                pt += '%' + precision[j]
-                if j != len(data) - 1:
-                    dt += ', '
-                    pt += ' & '
-                else:
-                    dt += ')'
-                    if box == 'none' or box == 'ver':
-                        pt += ' \\\\\n'
-                    else:
-                        pt += ' \\\\\n\\hline\n'
-            cadena += pt % eval(dt)
 
-    elif shape == 'hor':
-        if box == 'none' or box == 'hor':
-            disp += '{|c|' + 'c' * len(data) + '|}'
-        else:
-            disp += '{|c' + '|c' * len(data) + '|}'
-        cadena += disp + '\n\\hline\n'
+                precision = \
+                    prec[j] if (type(prec) == list \
+                        and len(prec) != 1) \
+                    else (prec if type(prec) == int \
+                        else prec[0]) # list format or single value
+
+                if kwargs['exp'] == True:
+                    exp_precision = \
+                        exp_prec[j] if (type(exp_prec) == list \
+                            and len(exp_prec) != 1) \
+                        else (exp_prec if type(exp_prec) == int \
+                            else exp_prec[0]) # list format or single value
+
+                num = round(data[j][i], precision)
+
+                formatting = ': .{}{}'.format( \
+                    '0' if (precision <= 0 and kwargs['exp'] == False) \
+                        else (precision if kwargs['exp'] == False \
+                            else exp_precision),
+                    'e' if kwargs['exp'] == True else 'f'
+                    )
+
+                row += '{{{}}}'.format(formatting).format(num)
+
+                if j != len(data)-1:
+                    row += ' & '
+
+            row += '\\''\\''\n'
+
+            if i != len(data[0])-1:
+                row += \
+                    '\\hline\n' * (kwargs['sep'] in ('horizontal''||''full'))
+
+            rows += row
+
+        body = disp + titles + rows
+
+
+    elif kwargs['shape'] in 'horizontal':
+
+        disp = '' \
+            + '{' \
+            + '|' * (kwargs['sep'] in ('vertical''||''full')) \
+            + 'c|' \
+            + ('c' + '|' * (kwargs['sep'] in ('vertical''||''full'))) \
+            * len(data) \
+            + '}'
+
+        rows = '' \
+            + '\n' \
+            + '\\hline\n' * (kwargs['sep'] in ('horizontal''||''full'))
+
         for i in range(len(data)):
-            dt = '('
-            pt = data_titles[i] + ' & '
+            row = data_titles[i] + ' & '
             for j in range(len(data[0])):
-                dt += 'data[' + str(i) + '][' + str(j) + ']'
-                pt += '%' + precision[i]
-                if j != len(data[0]) - 1:
-                    dt += ', '
-                    pt += ' & '
-                else:
-                    dt += ')'
-                    if box == 'none' or box == 'ver':
-                        pt += ' \\\\\n'
-                    else:
-                        pt += ' \\\\\n\\hline\n'
-            cadena += pt % eval(dt)
 
-    if box == 'hor' or box == 'full':
-        cadena += '\\end{tabular}\n\\end{table}\n'
-    else:
-        cadena += '\\hline\n\\end{tabular}\n\\end{table}\n'
+                precision = \
+                    prec[i] if (type(prec) == list \
+                        and len(prec) != 1) \
+                    else (prec if type(prec) == int \
+                        else prec[0]) # list format or single value
 
-    display(Latex(cadena))
+                if kwargs['exp'] == True:
+                    exp_precision = \
+                        exp_prec[i] if (type(exp_prec) == list \
+                            and len(exp_prec) != 1) \
+                        else (exp_prec if type(exp_prec) == int \
+                            else exp_prec[0]) # list format or single value
 
-    return
+                num = round(data[i][j], precision)
+
+                formatting = ': .{}{}'.format( \
+                    '0' if (precision <= 0 and kwargs['exp'] == False) \
+                        else (precision if kwargs['exp'] == False \
+                            else exp_precision),
+                    'e' if kwargs['exp'] == True else 'f'
+                    )
+
+                row += '{{{}}}'.format(formatting).format(num) # add num
+
+                if j != len(data[0])-1:
+                    row += ' & '
+
+            row += '\\''\\''\n' \
+
+            if i != len(data)-1:
+                row += \
+                    '\\hline\n' * (kwargs['sep'] in ('horizontal''||''full'))
+            # complete row
+
+            rows += row # add row
+
+        body = disp + rows
+
+    tail = '' \
+        + '\\hline\n' * (kwargs['sep'] in ('horizontal''||''full')) \
+        + '\\end{tabular}\n' \
+        + '\\caption{{{}}}\n'.format( \
+            (str(kwargs['caption']) * (type(kwargs['caption']) == str))) \
+            * (kwargs['caption'] != False) \
+        + '\\label{{{}}}\n'.format( \
+            (str(kwargs['label']) * (type(kwargs['label']) == str))) \
+            * (kwargs['label'] != False) \
+        + '\\end{table}\n'
+
+    table = head + body + tail
+
+    return print(table)
+
+# should I make formatting have space for positive numbers or remove it?
+# should I add an extra space on row start and end? there is a space
+# after and before & signs, so I should probably do that
+
+
+#### Test ####
+
+# col1 = [1.00923, 222.123, 311111.1, 3241]
+# col2 = [4.3232, 5.9, 6.3, 40004]
+# col3 = [0.001, 0.002, 0.003, 0.006]
+# titles = ['col1', 'col2', 'col3']
+#
+# table([col1, col2, col3], titles, shape = 'v', sep = 'v', exp = True)
