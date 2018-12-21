08d0d08dc98c436ab214bffac96f7745375c0887
Mario García <mariogarcc@gmail.com>
2018-12-21 21:00:13 +0100

    Moving test to test file in folder.

--- pylabutils/table.py
+++ pylabutils/table.py
@@ -286,16 +286,3 @@ def table(data, data_titles, **options):
 # should I make formatting have space for positive numbers or remove it?
 # should I add an extra space on row start and end? there is a space
 # after and before & signs, so I should probably do that
-
-
-#### Test ####
-
-col1 = [1.00923, 222.123, 311111.1, 3241]
-col2 = [4.3232, 5.9, 6.3, 40004]
-col3 = [0.001, 0.002, 0.003, 0.006]
-titles = ['col1', 'col2', 'col3']
-
-table([col1, col2, col3], titles, shape = 'v', sep = 'v',
-    exp = [True, True, False],
-    exp_prec = [0, 2, 3],
-)
