57dd1a5213a19fb0e2bb51d4d00dae43815f9b87
Mario García <mariogarcc@gmail.com>
2019-01-02 03:32:12 +0100

    Updated __init__ to comply chosen package layout

--- pylabutils/__init__.py
+++ pylabutils/__init__.py
@@ -1,3 +1,8 @@
-__all__ = ['read_data', 'fit', 'odr_fit', 'multisort', 'wdir', 'table']
-# also include (operations)
-# exclude est_correction, study, guessfinder
+from . import numfit
+from .numfit import *
+from . import utils
+from .utils import *
+
+__all__ = []
+__all__ += numfit.__all__
+__all__ += utils.__all__
