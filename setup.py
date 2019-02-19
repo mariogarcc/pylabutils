c6af49e30bde6c121ea84b20d00da8009935b5df
Mario García <mariogarcc@gmail.com>
2019-02-19 17:44:52 +0100

    1.0.2 with fixes to last fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name = 'pylabutils',
-    version = '1.0.1',
+    version = '1.0.2',
     author = 'Mario García',
     author_email = 'mariogarcc@gmail.com',
     description = 'Some utils for working with python'
